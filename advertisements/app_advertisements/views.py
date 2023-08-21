e63ac3125e3fa1a42cfc5a901cb3d324c06a80e2
Игорь <tokarev-igorek07@mail.ru>
2023-08-21 21:15:51 +0500

    Коды

--- advertisements/app_advertisements/views.py
+++ advertisements/app_advertisements/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('Успшно! Это главная страница')
+    return render(request, 'index.html')
 
 
 def page1(request):
