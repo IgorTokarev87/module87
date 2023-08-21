1d76bb90524c2ae9960567b3a00c720b51e07a8b
Игорь <tokarev-igorek07@mail.ru>
2023-08-21 23:02:47 +0500

    Лекция 5

--- advertisements/app_advertisements/views.py
+++ advertisements/app_advertisements/views.py
@@ -6,5 +6,9 @@ def index(request):
     return render(request, 'index.html')
 
 
+def top_sellers(request):
+    return render(request, 'top-sellers.html')
+
+
 def page1(request):
     return HttpResponse('Успшно! Это обычная страница')
